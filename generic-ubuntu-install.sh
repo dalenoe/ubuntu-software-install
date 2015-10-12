bcc0746012a966c2c39e2a2880164509d95586ef
Dale Noe <dino@dinosworkshop.com>
2015-10-11 21:14:08 -0500

    updating sources/packages

---
+++ generic-ubuntu-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script was written by Dale Noe (dalenoe@gmail.com)
+# It's a script that installs various software
+# and even makes a few changes.
+
+sudo apt-get update
+
+echo 'Ok.. now our packages are up to date'
+
+
