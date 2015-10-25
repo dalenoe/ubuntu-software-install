8c2a5bf781528da7bcc4be3e76267aa8287efcd1
Dale Noe <dino@dinosworkshop.com>
2015-10-25 07:05:05 -0500

    removed snail mail address from script header

--- generic-software-install.sh
+++ generic-software-install.sh
@@ -16,10 +16,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-#
 
 
 PASSWORD=$(whiptail --title "Root Password" --passwordbox "Enter your password and choose Ok to continue. We need the ROOT password for sudo commands." 10 60 3>&1 1>&2 2>&3)
