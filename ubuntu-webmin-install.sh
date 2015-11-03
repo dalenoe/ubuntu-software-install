c86f7366b7f1860e9f45bc4653592a963f77d592
Dale Noe <dino@dinosworkshop.com>
2015-11-02 21:52:00 -0600

    seems to be ok, will do one more check before merging into develop

--- ubuntu-webmin-install.sh
+++ ubuntu-webmin-install.sh
@@ -17,13 +17,23 @@ if [ $exitstatus = 0 ]; then
 
 	sleep 2s
 
+	whiptail --title "Webmin Install" --msgbox "We are about to install webmin. In the process a file (/etc/apt/sources.list.d/webmin.list) will be created for the webmin repository." 8 78
+
+        echo "Ok, we are going to create the webmin.list file along with downloading the key"
+
+	echo $PASSWORD | sudo bash
+
+	sudo touch /etc/apt/sources.list.d/webmin.list |  echo -e "deb http://download.webmin.com/download/repository sarge contrib\ndeb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
+
+	cd /root | wget http://www.webmin.com/jcameron-key.asc | sudo -S apt-key add jcameron-key.asc
+
 	echo "Now going to update the OS package manager"
 
 	echo "This is going to take awhile, go get a cup of coffee."
 
 	sleep 1s
-
-	echo $PASSWORD | sudo -S apt-get update
+#commented out for developing
+#	echo $PASSWORD | sudo -S apt-get update
 
 	sleep 1s
 
@@ -35,9 +45,7 @@ if [ $exitstatus = 0 ]; then
 
 	sleep 2s
 
-	whiptail --title "Webmin Install" --msgbox "We are about to install webmin. In the process a file (/etc/apt/sources.list.d/webmin.list) will be created for the webmin repository." 8 78
-
-	echo "Ok, we are going to create the webmin.list file along with downloading the key"
+	echo $PASSWORD | sudo -S apt-get install webmin
 
 else
 
